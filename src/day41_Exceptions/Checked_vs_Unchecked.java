616d459e89ec774cbf6b83aaeb23008b03a254be
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-08-14 21:31:44 +0200

    tracking

--- src/day41_Exceptions/Checked_vs_Unchecked.java
+++ src/day41_Exceptions/Checked_vs_Unchecked.java
@@ -1,6 +1,62 @@
 package day41_Exceptions;
 
+import day39_Recap.cydeoTask.Student;
+
+import java.io.FileInputStream;
+
 public class Checked_vs_Unchecked {
 
+    public static void main(String[] args) {
+
+        //unchecked exception:
+
+        int a = 10;
+        int b = 0;
+
+
+
+       // System.out.println(a/b); //ArithmeticException, terminates the program and doesn't allow the print statement "Wooden Spoon" execute
+       // System.out.println("Wooden Spoon");
+
+        char [] characters = {'A','B','C'};
+                    //         0   1   2
+
+        //System.out.println(characters[99]);//ArrayIndexOutOfBoundsException: Index 99 out of bounds for length 3
+
+
+        Student student = null;
+
+       // System.out.println( student.getName() );  //NullPointerException
+        //student.study();
+
+        final String str = "Wooden Spoon";
+
+         //str = null;
+
+       // System.out.println( str.toUpperCase() ); //NullPointerException
+
+        String str2 = ""; // object != null
+        System.out.println(str2.isEmpty()); //true
+
+
+        // checked Exception:
+
+        System.out.println("Hello");
+
+      //  Thread.sleep(3000);
+
+        System.out.println("Cydeo");
+
+     //   FileInputStream file = new FileInputStream("path of the file");
+
+
+
+
+
+
+
+
+
+    }
 
 }
---
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -0,0 +1,76 @@
+package day41_Exceptions;
+
+public class TryCatchBlocks {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test started");
+
+        try {
+            System.out.println(9/0); //ArithmeticException: / by zero
+            System.out.println("Try Block");
+
+        }catch (ArithmeticException e){
+
+            System.out.println("Catch Block");
+
+            System.out.println("Arithmetic Exception was occurred");
+        }
+
+
+
+        System.out.println("Test1 Completed");
+
+        System.out.println("----------------------------------");
+
+        System.out.println("Test2 started");
+
+        int[] numbers = {1,2,3,4,5};
+
+
+        try {
+
+            System.out.println(numbers[200]);
+
+            System.out.println("Try Block");
+
+        }catch (RuntimeException e){
+
+            e.printStackTrace(); // prints a stack trace (full details) of the exception
+
+            // System.out.println( e.getMessage() );//returns only brief description of the exception
+
+        }
+
+
+
+        System.out.println("Test2 is completed");
+
+        System.out.println("--------------------------------------------------");
+
+        System.out.println("Test3 started");
+
+        try {
+            System.out.println("Cydeo".substring(2, 0));
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Test3 is completed");
+
+
+        System.out.println("--------------------------------------------------");
+
+
+        System.out.println("Hello");
+
+
+
+
+
+
+
+
+
+    }
+}
