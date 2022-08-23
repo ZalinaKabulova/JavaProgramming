9f4799d2818dd318243f9903c4dad35a36975f6f
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-08-23 23:18:43 +0200

    tracking

--- src/day42_ExceptionsContinious/MorningWorkOut.java
+++ src/day42_ExceptionsContinious/MorningWorkOut.java
@@ -40,10 +40,9 @@ public class MorningWorkOut {
         }
 
     }
-
-
-
-
-
-    }
-
+}
+/*
+MorningWorkOut:
+			1. Do 30 push-ups and try pausing 1.5 seconds in each
+			2. Do 20 Pull-ups and try pausing 2.5 seconds in each
+ */
